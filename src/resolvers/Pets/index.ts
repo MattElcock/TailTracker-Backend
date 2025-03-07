ad1aaf2ca45fe30cc7dc7b8b478b6a8af9b0a51b
Matt Elcock <matthew.elcock82@googlemail.com>
2025-03-07 16:31:24 +0000

    refactor: move pet files

--- src/resolvers/Pets/index.ts
+++ src/resolvers/Pets/index.ts
@@ -1,8 +1,8 @@
 import { createPet } from "./mutations/createPet";
 import { enclosure } from "./queries/enclosure";
 import { pets } from "./queries/pets";
-import { subtype } from "./subtype";
-import { type } from "./type";
+import { subtype } from "./queries/subtype";
+import { type } from "./queries/type";
 
 const resolvers = {
   Query: {
