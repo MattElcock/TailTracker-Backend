0f32a53f5418131f7fa1ce0eaf9df015ab03b479
Matt Elcock <matthew.elcock82@googlemail.com>
2025-04-01 20:07:32 +0100

    chore: support for hosted db

--- src/knexClient.ts
+++ src/knexClient.ts
@@ -1,14 +1,35 @@
 import knex from "knex";
+import dotenv from "dotenv";
 
-const client = knex({
-  client: "postgresql",
-  connection: {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "tailtracker",
-    port: 5432,
+dotenv.config();
+
+const environment = process.env.NODE_ENV || "development";
+
+const config = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "tailtracker",
+      port: 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
-});
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+};
+
+const client = knex(config[environment]);
 
 export { client };
