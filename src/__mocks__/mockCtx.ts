7bb4259b568881d5d2777fc2ff6f29a1c251358c
Matt <matthew.elcock82@googlemail.com>
2025-09-27 16:18:08 +0100

    Test/resolvers enclosures (#36)
    * test: resolves-Enclosures
    
    * refactor: test block names
    
    * fix: remove redundant comment

---
+++ src/__mocks__/mockCtx.ts
@@ -0,0 +1,10 @@
+const mockCtx = {
+  user: {
+    id: "def",
+    first_name: "Joe",
+    last_name: "Bloggs",
+    seen_app_purpose_disclaimer: "123",
+  },
+};
+
+export default mockCtx;
---
+++ src/resolvers/Enclosures/queries/__tests__/pets.test.ts
@@ -0,0 +1,52 @@
+import mockCtx from "@/__mocks__/mockCtx.js";
+import { listPetsDb } from "@/db/Pets/listPetsDb.js";
+import { describe, expect, Mock, test, vi } from "vitest";
+import { pets } from "../pets.js";
+
+vi.mock("@/db/Pets/listPetsDb.js", () => ({
+  listPetsDb: vi.fn(),
+}));
+
+describe("Field Resolver / pets", () => {
+  test("calls the database layer successfully", async () => {
+    (listPetsDb as Mock).mockResolvedValue([
+      {
+        id: "bono-id",
+        name: "Bono",
+        subtype_id: "lab-id",
+        enclosure_id: "house-id",
+      },
+      {
+        id: "blue-id",
+        name: "Blue",
+        subtype_id: "springerspaniel-id",
+        enclosure_id: "house-id",
+      },
+    ]);
+
+    const response = await pets(
+      {
+        id: "house-id",
+        type: "house",
+        name: "Doggo Palace",
+      },
+      undefined,
+      mockCtx
+    );
+
+    expect(response).toEqual([
+      {
+        enclosure: "house-id",
+        id: "bono-id",
+        name: "Bono",
+        subtype: "lab-id",
+      },
+      {
+        enclosure: "house-id",
+        id: "blue-id",
+        name: "Blue",
+        subtype: "springerspaniel-id",
+      },
+    ]);
+  });
+});
