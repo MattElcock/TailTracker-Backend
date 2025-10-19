17d14041dae2d70558c88f103a2a14272f83b2fe
Matt Elcock <matthew.elcock82@googlemail.com>
2025-10-19 10:32:11 +0100

    fix: import correct resolver@

--- src/resolvers/Enclosures/index.ts
+++ src/resolvers/Enclosures/index.ts
@@ -1,7 +1,7 @@
-import { type } from "os";
 import { createEnclosure } from "./mutations/createEnclosure.js";
 import { enclosures } from "./queries/enclosures.js";
 import { pets } from "./queries/pets.js";
+import { type } from "./queries/type.js";
 
 const resolvers = {
   Query: {
